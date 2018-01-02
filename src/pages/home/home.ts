ca42f0898e9430a432458a121881ba94e0950f82
taoqun <taoqun@317hu.com>
2018-01-02 09:25:04 +0800

    页面跳转，参数添加测试

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,6 +12,7 @@ export class HomePage {
   recyclePage: Object;
   sortingScanPage: any;
   washingFreePage: any;
+  params: object = { test: 'i am is kaka' };
   constructor(public navCtrl: NavController) {
     this.recyclePage = RecycleIndexPage;
     this.sortingScanPage = SortingScanPage;
